924a84226d12ec35d8ca4e510fbc2512e7628ab8
Andres Arias & Thierry Moudiki <pair+aarias89.thierrymoudiki@devbootcamp.com>
2017-08-30 11:22:41 -0400

    finish releases 0 and 1

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,26 @@
 def iterative_nth_fibonacci_number(n)
+
+  first = 0
+  second = 1
+   return first if n == 0
+   counter = 1
+  while counter <  n
+    temp = second
+    second = first + temp
+    first = temp
+    counter += 1
+  end
+  second
+
 end
 
 def recursive_nth_fibonacci_number(n)
+  case n
+    when 0
+      return 0
+    when 1
+      return 1
+  end
+
+  recursive_nth_fibonacci_number(n-1) + recursive_nth_fibonacci_number(n-2)
 end
