3e652892bd44880e201dff9c5660189944acc15f
Amber Maillard & Chris Bordes & klorealf & Samory Harris <pair+amber2394.cbordes8.klorealf.samory070@devbootcamp.com>
2017-08-30 12:15:29 -0400

    make iterative and recursive code to file

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,36 @@
 def iterative_nth_fibonacci_number(n)
+#  a = 0
+#  b = 1
+#  while b < n do
+#    puts b
+#    a = b
+#    b = a + b
+#  end
+#end
+
+if n == 0
+  return 0
+elsif n == 1
+  return 1
+else
+  fib = [0,1]
+
+  while n >= fib.length
+   fib << fib[(fib.length-1)] + fib[(fib.length-2)]
+
+  end
+  fib.pop
+  end
 end
+p iterative_nth_fibonacci_number(5)
+
 
 def recursive_nth_fibonacci_number(n)
+  return 0 if n == 0
+    if n == 1
+      return 1
+    end
+  recursive_nth_fibonacci_number(n-1) + recursive_nth_fibonacci_number(n -2)
 end
+
+
