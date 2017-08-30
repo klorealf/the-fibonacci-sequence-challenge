439c644ef988e1d4779f35e21aa7f1f35510fe21
Catherine Rice & David Skaggs <pair+clr2107.davidtskaggs@devbootcamp.com>
2017-08-30 11:26:32 -0400

    Add iterative fib method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,17 @@
 def iterative_nth_fibonacci_number(n)
+  counter = 0
+  fib_array = [0,1]
+  until counter == n - 1
+  fib_array << fib_array[-1] + fib_array[-2]
+  counter+=1
+end
+p fib_array[-1]
 end
 
+
+
 def recursive_nth_fibonacci_number(n)
+
 end
+
+
